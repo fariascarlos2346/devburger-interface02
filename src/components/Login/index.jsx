a68aa86526fd43dac68bb061b4be2f4c96974ae3
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-06-08 21:25:11 -0300

    estrutura HTML

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,31 @@
-
-
+import { Container } from './styles';
+import Logo from '../../assets/logo.svg';
 export function Login() {
     return (
-        <div>
-            <h1>Login Bia e Lulu</h1>
-        </div>
+        <Container>
+            <leftContainer>
+                <img src={Logo} alt="logo-devburger"/>
+            </leftContainer>
+            <RightContainer>
+                <Title>
+                    Olá, seja bem vindo ao<span> Pitada do Sabor! </span>  Acesse com seu 
+                    <span>Login e Senha.</span>
+                </Title>
+                <Form>
+                    <InputContainer>
+                        <label>Email</label>
+                        <input type="email" />
+                    </InputContainer>
+
+                    <InputContainer>
+                        <label>Senha</label>
+                        <input type="password" />
+                        <Link>Esqueci minha senha.</Link>
+                        <Button>Entrar</Button>
+                    </InputContainer>
+                </Form>
+                <Link>Não possoi conta? Clique aqui.</Link>
+            </RightContainer>
+        </Container>
     );
 };
